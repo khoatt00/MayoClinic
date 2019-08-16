f55d844f9012d768361cc2efe529b8ddce089ad1
ujjwalsingh075 <ujjwalsingh075@gmail.com>
2019-08-15 23:28:53 -0500

    Setting Up workspace for Maatic IT Timesheet Backend

---
+++ Backend/src/main/java/com/maaticit/timesheet/main/MaaticTimesheetApplication.java
@@ -0,0 +1,15 @@
+package com.maaticit.timesheet.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaticTimesheetApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MaaticTimesheetApplication.class, args);
+
+	}
+
+}
