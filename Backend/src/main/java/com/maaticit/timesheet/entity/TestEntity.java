835c38730eae3e138ed41b45b48f2c6969c5cdfa
uzwolsingh075 <ujjwalsingh075@gmail.com>
2019-08-17 17:55:12 -0500

    Created Test API (#9)

---
+++ Backend/src/main/java/com/maaticit/timesheet/entity/TestEntity.java
@@ -0,0 +1,5 @@
+package com.maaticit.timesheet.entity;
+
+public class TestEntity {
+
+}
---
+++ Backend/src/main/java/com/maaticit/timesheet/service/TestService.java
@@ -0,0 +1,6 @@
+package com.maaticit.timesheet.service;
+
+public interface TestService {
+
+	public void getTestEntities();
+}
