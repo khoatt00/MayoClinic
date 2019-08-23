81f4bb7c77d811f24a218624a7ac76687710f279
Sandeep Dhital <dhitalsand@gmail.com>
2019-08-22 19:51:07 -0500

    added dto class and done some work in testing

---
+++ Backend/src/main/java/com/maaticit/timesheet/dto/EmployeeDto.java
@@ -0,0 +1,42 @@
+package com.maaticit.timesheet.dto;
+	
+	public class EmployeeDto {
+		private int id;
+		private String name;
+		private String phone;	
+		private String username;
+		private String password;
+	
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	
+	
+	}
