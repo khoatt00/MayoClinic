0337c5bbd6c0e274faac7429dc54fbe6cb007313
AgeGrace <agegrace1@gmail.com>
2019-08-29 19:08:20 -0500

    Mtm 13 1 (#19)
    * Created a navigation for maatic timesheet
    
    * -worked on user story MTM-13
    
    * resolved conflicts

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { ListUserComponent } from './registration/listusers.component';
+import { AddTimesheetComponent } from './timesheet/addtimesheet.component';
+
+const routes: Routes = [
+  { path: 'User/AddUser', component: RegistrationComponent },
+  { path: 'User/Listusers', component: ListUserComponent },
+  { path: 'Timesheet/add', component: AddTimesheetComponent },
+  { path: 'Timesheet/AddTimesheet', component: AppComponent },
+  { path: '', redirectTo: 'User/AddUser', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, RegistrationComponent, ListUserComponent,
+    AddTimesheetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ FrontEnd/src/app/timesheet/addtimesheet.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './addtimesheet.component.html', 
+    styleUrls: ['./timesheet.component.css']
+})
+export class AddTimesheetComponent {
+
+}
